7cab17a83606e007ff39c0f19a494b7aef9f6ab5
sh3oo6 <141688473+sh3oo6@users.noreply.github.com>
2023-08-16 02:46:01 +0300

    Delete Dex4.py

--- Dex4.py
+++
@@ -1,8 +0,0 @@
-from Dex3 import *
-id='5229914714'
-token='6183317549:AAGB5jAoTSGSc0M0Y_8WR7kEa7oYbyjshM0'
-led=TelegramClient('dex35', 2192036, '3b86a67fc4e14bd9dcfc2f593e75c841')
-led.start()
-led.send_message('me','ok')
-uss='qwertyuioplkjhgfdsazxcvbnm1234567890'
-rr='qwertyuioplkjhgfdsazxcvbnm'
