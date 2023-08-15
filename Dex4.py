72b797549a7e231120707e7349dbeae6bb9a0a6d
sh3oo6 <141688473+sh3oo6@users.noreply.github.com>
2023-08-16 02:46:30 +0300

    Add files via upload

---
+++ Dex4.py
@@ -0,0 +1,14 @@
+from time import sleep
+import requests
+from telethon.tl.functions.account import UpdateUsernameRequest
+from telethon.sync import TelegramClient , functions , events
+import asyncio
+import random
+import os
+id='5229914714'
+token='6183317549:AAGB5jAoTSGSc0M0Y_8WR7kEa7oYbyjshM0'
+led=TelegramClient('dex35', 2192036, '3b86a67fc4e14bd9dcfc2f593e75c841')
+led.start()
+led.send_message('me','ok')
+uss='qwertyuioplkjhgfdsazxcvbnm1234567890'
+rr='qwertyuioplkjhgfdsazxcvbnm'
